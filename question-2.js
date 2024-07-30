1022726d133e5579b49495f61eb96523ef75f0af
nueng <166599504+imperresa@users.noreply.github.com>
2024-07-30 09:50:36 +0700

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -1,5 +1,25 @@
 function findProductPrice(products, name) {
   // Your code here
+  // for(let i of products){
+  //   if(i.name === name ){
+  //     return i.price
+  //   }
+  // }
+  // return -1
+  let left = 0
+  let right = products.length - 1
+  let index = -1
+  while(left<=right){
+    let mid = Math.floor((left+right)/2)
+    if(products[mid].name===name){
+      return products[mid].price
+    }else if(products[mid].name < name){
+      left = mid +1
+    }else{
+      right = mid -1
+    }
+  }
+  return index
 }
 
 // Test case
